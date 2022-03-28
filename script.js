994c655b3abc6b1fae7b4cedf75bff3520d7e088
artgalla8 <artgalla8@gmail.com>
2022-03-28 20:13:59 +0100

    javascript finished

--- script.js
+++ script.js
@@ -17,3 +17,56 @@ start.addEventListener("click", function() {
    alert("Timer is already running");
  }
 });
+
+// reset all functions
+reset.addEventListener("click",function() {
+  workMins.innerText = 25;
+  workSecs.innerText = 00;
+
+  breakMins.innerText = 5;
+  breakSecs.innerText = 00;
+  document.getElementById("counter").innerText = 0;
+  stopInterval()
+  startTimer = undefined;
+})
+
+stop.addEventListener("click", function() {
+  stopInterval()
+  startTimer = undefined;
+})
+
+// start timer function
+function timer() {
+
+//work timer countdown
+  if (workSecs.innerText != 0) {
+    workSecs.innerText --;
+  } else if (workMins.innerText != 0 && workSecs.innerText == 0) {
+    workSecs.innerText = 59;
+    workMins.innerText --;
+  }
+
+//break timer countdown
+  if (workMins.innerText == 0 && workSecs.innerText == 0) {
+    if(breakSecs.innerText != 0){
+      breakSecs.innerText--;
+  } else if(breakMins.innerText != 0 && breakSecs.innerText == 0){
+      breakSecs.innerText = 59;
+      breakMins.innerText--;
+    }
+  }
+// increment counter by one if one cycle is completed
+  if(workMins.innerText == 0 && workSecs.innerText == 0 && breakMins.innerText == 0 && breakSecs.innerText == 0) {
+    workMins.innerText = 25;
+    workSecs.innerText = 00;
+
+    breakMins.innerText = 5;
+    breakSecs.innerText = 00;
+
+    document.getElementById("counter").innerText++;
+  }
+}
+//stop timer function
+function stopInterval() {
+  clearInterval(startTimer);
+}
